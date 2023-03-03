16386c098c133b351a8ca9839b9da94badcff4fe
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2023-03-02 19:52:48 -0600

    Delete Mutacion.m

--- Mutacion.m
+++
@@ -1,9 +0,0 @@
-function y = Mutacion(x,probabilidad_mutacion,sigma)
-
-  pos_mutacion = (rand(size(x)) < probabilidad_mutacion);
-  y = x;
-  r = randn(size(x));
-  y(pos_mutacion) = x(pos_mutacion) + sigma*r(pos_mutacion);
-    
-  
-end
